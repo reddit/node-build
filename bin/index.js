310d5baa9c3757d0261edafbcde38bd21772a382
Ryan Schwers <schwers.r@gmail.com>
2016-04-20 19:57:01 -0700

    Add default configs and flesh out the install script

--- bin/index.js
+++ bin/index.js
@@ -3,23 +3,45 @@ var fs = require('fs');
 var path = require('path');
 var build = require('../lib/build');
 var makeBuild = require('../lib/makeBuild').makeBuild;
+var configs = require('../lib/configs');
 
 var argv = require('yargs')
-  .alias('r', 'rawconfig')
-  .describe('r', 'path to a raw-config via a node file with moduel.exports = config')
-  .default('r', './blueprints.config.js')
+  .alias('b', 'blueprintsPath')
+    .describe('b', 'path to a raw-config via a node file with moduel.exports = config')
+    .default('b', './blueprints.config.js')
+  .alias('p', 'production')
+    .describe('p', 'enable production settings for the default build cofings')
+    .default('p', false)
+  .alias('c', 'client')
+    .describe('c', 'use the default client build, assumes you have an entry point to a client at ~/lib/client.[some es6.js or .js or .jsx]')
+    .default('c', false)
+  .alias('s', 'server')
+    .describe('s', 'use the default server build, assumes you have an entry point to a server at ~/lib/server[some es6.js or .js or .jsx]')
+    .default('s', false)
+  .alias('b', 'both')
+    .describe('b', '[DEFAULT=true] use both a client and a server build. checks if you have an extend build and applies it.')
+    .default('b', true)
+  .alias('w', 'watch')
+    .describe('w', '[DEFAULT=false] force watching of all builds')
+    .default('w', false)
+  .alias('i', 'ignoreBlueprints')
+    .describe('ignore the blueprints.config.js file in the current directory and use defaults')
+    .default('i', false)
   .argv;
 
 console.log('starting');
 console.log('cwd', process.cwd());
 
-function loadRawConfigModule(configPath, extensions) {
+function loadBuildsFromPath(configPath) {
   var builds = require(path.resolve(configPath));
   if (!Array.isArray(builds)) {
+    if (builds.extensions === true) {
+      return { extensions: _.omit(builds, 'extensions') };
+    }
     builds = [builds];
   }
 
-  return makeConfig(applyExtensions(builds, extensions));
+  return { builds }
 }
 
 function applyExtensions(builds, extensions) {
@@ -27,16 +49,41 @@ function applyExtensions(builds, extensions) {
   return builds.map(function(build) { return _.extend(build, ext); });
 }
 
-function makeConfig(builds) {
-  return { builds: builds.map(makeBuild) };
+function makeConfig(builds, extensions) {
+  return { builds: applyExtensions(builds, extensions).map(makeBuild) };
 }
 
-function loadConfigFromPath(path, extensions) {
-  return loadRawConfigModule(path, extensions);
+var builds = [];
+var extensions = {};
+
+if (argv.blueprintsPath && !argv.ignoreBlueprints) {
+  var blueprints = loadBuildsFromPath(configPath);
+  if (blueprints.extensions) {
+    extensions = blueprints.extensions;
+  } else if (blueprints.builds.length) {
+    builds = blueprints.builds;
+  }
 }
 
-if (argv.rawconfig) {
-  var config = loadConfigFromPath(argv.rawconfig);
-  build(config);
-  return;
+function loadDefaultConfigs() {
+  if (argv.client) {
+    builds = [ configs.getClientConfig(argv.production) ];
+  } else if (argv.server) {
+    builds = [ configs.getServerConfig(argv.production) ];
+  } else if (argv.both) {
+    builds = [
+      configs.getClientConfig(argv.production),
+      configs.getServerConfig(argv.production),
+    ];
+  }
 }
+
+if (!builds.length) {
+  loadDefaultConfigs();
+}
+
+if (argv.watch) {
+  extensions.watch = true;
+}
+
+build(makeConfig(builds, extensions));
--- examples/client/client.es6.js
+++ examples/client/client.es6.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import lodash from 'lodash'; // also unused
+/*
+  try building with 'blueprints -i --client --production'
+    : -w || --watch if you want to experiment
 
-const Hellow = (props) => {
+  if you're developing blueprints:
+      build with something like node ../../bin/index.js -i -c -p -w
+
+  The file size for this minifed, with React, React Dom, Lodash, and owrn own code is 141KB.
+  Note how the production minifed build of React Dom on Facebooks site is 143KB,
+  that's where most of the size comes from.
+
+  Lodash is being heavily tree-shaken in this example because of babel-plugin-lodash-loader
+  (it's enabled in /lib/generators/loaders/ESNextReactLoader.js). Without it this
+  build would be 190KB (minified, NOT gzipped).
+
+  Still looking into tree-shaking react-dom if possible but it seems unlikely.
+
+  The React components Unused and AsloUnused are removed as well automatically by
+  webpack's tree shaking.
+
+  For developing: you can use git@github.com:ksmith97/GzipSimpleHTTPServer.git
+  to run a very simple webserver that automatically gzips things.
+  Min & gzipped sizes comapred to reddit-mobile?
+  Reddit Mobile: 347 KB
+  This: 42KB
+*/
+import React from 'react';
+import ReactDom from 'react-dom';
+import { omit } from 'lodash'; // mostly unused, thanks to the babel-lodash-loader
+
+const Hello = (props) => {
   return (
     <div className='foo'>
       <div className='bar'>Hello</div>
+    </div>
+  );
+};
+
+const HelloWrapper = (props) => {
+  const ommited = omit(props, 'ommited');
+
+  return (
+    <div classname='foo'>
+      <Hello {...ommited} />
     </div>
   );
 };
@@ -25,4 +62,5 @@ const AlsoUnused = (props) => {
   );
 };
 
-React.render(<Hello />, document.getElementById('react-container'));
+console.log('rendering');
+ReactDom.render(<HelloWrapper />, document.getElementById('react-container'));
