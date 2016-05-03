83390a32fbc82505bdc2dd41a58b99bc1279702f
Ryan Schwers <schwers.r@gmail.com>
2016-05-03 14:53:32 -0700

    Fix buildWebfonts.js for r-example-app style directories

--- bin/buildWebfonts.js
+++ bin/buildWebfonts.js
@@ -3,18 +3,18 @@ var glob = require('glob');
 var path = require('path');
 var webfontsGenerator = require('webfonts-generator');
 
-var SRC = path.join(__dirname, '/assets/svg/*.svg');
+var SRC = path.resolve('./assets/svg/*.svg');
 
 glob(SRC, function(error, files) {
   console.log('generating font from:\n', files);
 
   webfontsGenerator({
     files: files,
-    dest: path.join(__dirname, '/assets/fonts'),
+    dest: path.resolve('./assets/fonts'),
     fontName: 'rfont',
     css: true,
-    cssDest: path.join(__dirname, '/lib/client/rfont.css'),
-    cssFontsPath: '/assets/fonts',
+    cssDest: path.resolve('./assets/fonts/rfont.css'),
+    cssFontsUrl: '/fonts',
     html: true,
     types: ['svg', 'ttf', 'woff', 'eot'],
     normalize: true,
